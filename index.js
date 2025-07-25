4a1231f3f45da22413ecad4f308a7517c9475b46
Ryan Stokes <ryanstokescfbre@gmail.com>
2025-07-24 19:54:49 -0500

    index.html and index.js linked index.js in the head of index.html added function parameters

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-function add(){
-
-}
-
-function subtract () {
-
-}
-
-function multiply () {
-
-}
-
-function divide () {
+function add(num1, num2){
   
 }
+
+add();
+
+function subtract (num1, num2) {
+
+}
+
+subtract();
+
+function multiply (num1, num2) {
+  console.log(num1 * num2);
+}
+
+multiply(2, 5);
+
+function divide (num1, num2) {
+  
+}
+
+divide();
