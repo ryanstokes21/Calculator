4f4197d2c0c0729c8446a5d2e9046cfe2bec0c17
Ryan Stokes <ryanstokescfbre@gmail.com>
2025-07-25 15:08:17 -0500

    index.js added all query selectors fixed function calls in operate(); started event listeners

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const displayValue = document.querySelector('#display');
+const operands = document.querySelectorAll('.operand');
+const operators = document.querySelectorAll('.operator');
+const equals = document.querySelector('#equal');
+const clear = document.querySelector('#clear');
+
 let currentOperand = 0;
 let previousOperand = 0;
 let operator = '';
@@ -20,7 +26,13 @@ function divide (num1, num2) {
 
 function operate (num1, num2, op) {
   if (op === '+') return add(num1, num2);
-  if (op === '-') return add(num1, num2);
-  if (op === '*') return add(num1, num2);
-  if (op === '/') return add(num1, num2);
+  if (op === '-') return subtract(num1, num2);
+  if (op === '*') return multiply(num1, num2);
+  if (op === '/') return divide(num1, num2);
 }
+
+operands.forEach(button => {
+  button.addEventListener('click', e => {
+    
+  })
+})
